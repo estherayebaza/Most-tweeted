69c0f807131bc9c0bcf8f29246fbd61b676333c5
esther ayebaza <estherayebaza@gmail.com>
2021-09-09 06:48:24 +0300

    first commit

---
+++ assignment.py
@@ -0,0 +1,31 @@
+from collections import Counter
+tweet_names = ["sachin tweet_id_1",
+			"sehwag tweet_id_2",
+			"sachin tweet_id_3",
+			"sachin tweet_id_4"]
+
+
+	
+uniq_names = [pref_names.split()[0] for
+			pref_names in tweet_names]
+
+times = Counter(uniq_names)
+repeat = times.values()
+
+for element in set(repeat):
+	duplicate = ([(key, value) for
+			key, value in sorted(times.items()) if
+			value == element])
+	
+	if len(duplicate) > 1:
+		for (key, value) in duplicate:
+			print (key,'',value)
+	max_value = max(times.values())
+	temp_max_result = [(key, value) for
+					key, value in sorted(times.items()) if
+					value == max_value]
+	
+	if temp_max_result != duplicate:
+		for (key,value) in temp_max_result:
+			print (key,'',value)
+			
